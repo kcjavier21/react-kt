4cfdf0cd24a280f15eb3d6107fb00789338013d2
Ken Javier <kencarlo21@gmail.com>
2024-11-06 19:13:50 +0800

    initial ui implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import fordMustang from './assets/images/2025_ford_mustang.avif'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="card" style={{ width: '24rem' }}>
+        <img src={fordMustang} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h3 className="card-title">2025 Ford Mustang</h3>
+          <h5>$33515.00</h5>
+          <p className="card-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio,
+            consectetur.
+          </p>
+          <a
+            href="https://www.edmunds.com/ford/mustang/"
+            target="_blank"
+            className="btn btn-primary"
+          >
+            Buy Now
+          </a>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
